375722b0b7a89d679b2f36f861a81e0e3b27e204
jo17-dev <procompter1@gmail.com>
2024-06-16 22:41:58 -0400

    updated addAccount and created uploadDatasToStream and downloadDatasFromStream

--- DataManager.java
+++ DataManager.java
@@ -8,27 +8,22 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
-// import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class DataManager {
     private String filepath;
     private File datasFile;
-    private ObjectOutputStream datasWriter;
-    private ObjectInputStream datasReader;
     private ArrayList<Account> datas;
 
     public DataManager(String filepath) throws FileNotFoundException, IOException, LockBoxException {
         this.filepath = filepath;
         this.datasFile = new File(this.filepath);
-        datasWriter = new ObjectOutputStream(new FileOutputStream(datasFile));
-        datasReader = new ObjectInputStream(new FileInputStream(datasFile));
-        datas = new ArrayList<Account>();
 
+        datas = downloadDatasFromStream(datasFile);
 
         // check if the file exists
-        if(!datasFile.isFile()){
+        if(!datasFile.isFile() || !datasFile.exists()){
             datasFile.createNewFile(); // ioexception
         }
     }
@@ -36,10 +31,15 @@ public class DataManager {
     // ajout d'un Account
     public void addAccount(Account acc) throws IOException{
         if(datasFile.canWrite()){
-            // ObjectOutputStream 
-            System.out.println(" ---------- Amorage du processus d'écriture --------");
 
-            datasWriter.writeObject((Account) acc);
+            System.out.println(" ---------- Amorage du processus d'écriture --------");
+            for(Account item : datas){
+                if(item.getLabel().equals(acc.getLabel())){ // if there is another accoun with the same lable, we just cancel it
+                    throw new IOException("Erreur de traitement: Le Ce compte existe déjas existe déjas");
+                }
+            }
+            datas.add(acc);
+            uploadDatasToStream(datasFile, datas);
             System.out.println(" ------- Fin du process d'écriture ------ ");
         }else{
             throw new IOException("Erreur l'ors de l'écriture des données dans un fichier");
@@ -48,32 +48,40 @@ public class DataManager {
 
     // Lecture des données d'un compte
     public ArrayList<Account> getAccounts (){
-        // ArrayList<Account> result = new ArrayList<Account>();
-        while (true) {
-            try{
-                Account premier = (Account) datasReader.readObject();
-                System.out.println(premier.toString());
-                datas.add(premier);
-            }catch(IOException ioe){
-                System.out.println("erreur de lecture - fin de la lecture");
-                break;
-            }catch(ClassNotFoundException cnfe){
-                System.out.println("Classe Account non trouvée");
-                break;
-            }
-        }
-        
-        // System.out.println(result.size());
-        return datas;
+        return downloadDatasFromStream(datasFile);
     }
 
-    // fermeture des streams:
-    public boolean closeStreams(){
+    private static void uploadDatasToStream(File dbFile, ArrayList<Account> accounts){
         try{
+           ObjectOutputStream datasWriter = new ObjectOutputStream(new FileOutputStream(dbFile));
+            for(Account item : accounts){
+                datasWriter.writeObject(item);
+            }
             datasWriter.close();
-            return false;
         }catch(IOException ioe){
-            return true;
+            System.out.println("Error while updating datas to database");
         }
+    }
+
+    private static ArrayList<Account> downloadDatasFromStream(File dbFile){
+        try{
+            ArrayList<Account> result = new ArrayList<Account>();
+            ObjectInputStream datasReader = new ObjectInputStream(new FileInputStream(dbFile));
+            while (true) {
+                try{
+                    result.add(
+                        (Account) datasReader.readObject()
+                    );
+                }catch(Exception exception){
+                    break;
+                }
+            }
+            datasReader.close();
+            return result;
+        }catch(Exception e){
+            
+            System.out.println("Downloading falied");
+        }
+        return new ArrayList<Account>();
     }
 }
